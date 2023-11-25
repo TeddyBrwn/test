b9db863cba7ec181f4abfcd3c0ccbd2d9f6ae79e
teddie brwn <teddiebrwn@192.168.1.128>
2023-11-25 15:29:15 +0700

    ok

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'custer0.f1zg7pt.mongodb.net',
+    DB_USER: 'admin',
+    DB_PASS: '123',
+    DB_DATABASE: 'shoppingonline',
+    JWT_SECRET: 'admin',
+    JWT_EXPIRES: '100000000000000000000000', // in milliseconds
+    EMAIL_USER: 'huynhdong1115@gmail.com', // gmail service
+    EMAIL_PASS: 'huynhdong0703645815'
+  };
+  module.exports = MyConstants;
